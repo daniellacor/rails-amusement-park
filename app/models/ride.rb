b6e32d7eaca85ffcabfffc1af8baddcb59bebf29
daniellacor <daniella.corricelli@gmail.com>
2016-05-09 17:48:30 -0400

    Attractions edit

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,9 +4,9 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
-    @user = User.find(params[:id])
-    user.tickets -= attraction.tickets
-    user.nausea -= attraction.nausea_rating
-    user.happiness -= attraction.happiness_rating
+    @user = User.find(session[:user_id])
+    @user.tickets -= @attraction.tickets
+    @user.nausea -= @attraction.nausea_rating
+    @user.happiness -= @attraction.happiness_rating
   end
 end
