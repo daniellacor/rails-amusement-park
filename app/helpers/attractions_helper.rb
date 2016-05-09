6f0fe4abedf084936622b138799910cea49ae393
alanasim <alana.simoncelli@gmail.com>
2016-05-09 18:59:02 -0400

    attractions updates

--- app/helpers/attractions_helper.rb
+++ app/helpers/attractions_helper.rb
@@ -11,14 +11,26 @@ module AttractionsHelper
     if user.admin
       link_to "Show #{attraction.name}", attraction_path(attraction)
     else
-      button_to "Go on #{attraction.name}", attraction_path(attraction)
+      button_to "Go on #{attraction.name}", attraction_path(attraction), method: :get
     end 
   end
 
   def admin_edit_attraction
-    user = User.find(session[:user_id])
-    if user.admin
+    @user = User.find(session[:user_id])
+    if @user.admin
       link_to "Edit Attraction", edit_attraction_path(@attraction)
+    else
+      form_for @user do |f|
+        hidden_field_tag('attraction_id', @attraction.id)
+        f.submit "Go on #{@attraction.name}"
+      end
+    end
+  end
+
+  def go_on_attraction_link(attraction)
+    @user = User.find(session[:user_id])
+    if @user.admin == false
+      link_to "Go on " + attraction.name, attraction_path(attraction)
     end
   end
 end
