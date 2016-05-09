4ae275f30f8e772b7dea6a91140647c1cd88fb3d
daniellacor <daniella.corricelli@gmail.com>
2016-05-09 13:48:04 -0400

    Built Models for users, rides, attractions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  validates_presence_of :name, :happiness, :height, :nausea, :tickets
+
 end
