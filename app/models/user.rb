c1118f96f7dce80041591630d441640fa806b54a
daniellacor <daniella.corricelli@gmail.com>
2016-05-09 14:29:46 -0400

    Added landing page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
 
-  validates_presence_of :name, :happiness, :height, :nausea, :tickets
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :users
+    resources :attractions
+  end
+
+  resources :users
+  resources :attractions
+
+  root 'users#index'
 
 end
