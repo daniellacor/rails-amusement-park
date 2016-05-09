981dd10c7e003a69772f27928d1487e154cc70f7
daniellacor <daniella.corricelli@gmail.com>
2016-05-09 16:24:09 -0400

    Added log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
-    @user = User.find(id: params[:id])
-    session[]
-    # current_user = @user.id
+    @user = User.find_by(name: params[:name])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 end
